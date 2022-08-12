1581b1c32fba1cfca88eb2fe2ff7238a0458fe25
Asherflo <aniyikayetemitopeflorence@gmail.com>
2022-08-12 16:45:18 +0100

    newly added

---
+++ src/views/dashboard/pages/events/MyEvent.js
@@ -0,0 +1,24 @@
+import React, {useEffect} from "react";
+import axios from "axios";
+
+const MyEvent = ({event}) => {
+
+
+    return(
+        <div className="event-cards">
+            <div className="image-containers" style={{backgroundImage:'url(https://images.pexels.com/photos/315755/pexels-photo-315755.jpeg?auto=compress&cs=tinysrgb&w=1600)' }}>
+            </div>
+            <div className="event-detail">
+                <p>Event Title</p>
+                <h5>{event.title}</h5>
+                <p>Event Title</p>
+                <h5>{event.description}</h5>
+            </div>
+            <div className="event-actions">
+                <button>View Details</button>
+            </div>
+        </div>
+    )
+}
+
+export default MyEvent
--- src/views/dashboard/pages/events/MyEvents.js
+++ src/views/dashboard/pages/events/MyEvents.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./myEvents.css"
+import MyEvent from "./MyEvent";
+import axios from "axios";
 
 const MyEvents = () => {
+    const [eventData,setEventData] =useState([])
+
+
+        useEffect(() =>{
+            axios.get("http://localhost:5000/fakeEvents").
+            then((result) =>setEventData(result.data))
+                .catch((error) => console.log(error))
+
+        },[])
     return(
         <div className="my-events-container">
             <div className="my-events-header">
                 <div className="my-events-header-filter">
                     <div>
                         <p>Filter By:</p>
+
                         <input type="date"/>
                     </div>
                     <div>
@@ -17,6 +29,13 @@ const MyEvents = () => {
                 </div>
 
                 <button>Create Event</button>
+            </div>
+            <div className="my-events-cards">
+                { eventData.length > 0
+                    ? eventData.map((event)=> <MyEvent event={event} /> )
+                    :<h5 style={{ textAlign : "center" }} >No Event</h5>
+                }
+
             </div>
         </div>
     )
